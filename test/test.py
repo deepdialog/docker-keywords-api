b4f7c3557c6abd226fd7cabc0a31033350006b24
Indigo <j.c.f.gauss@sjtu.edu.cn>
2021-11-04 20:54:31 +0800

    🔥🐛

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('..')
-from main import ht
+from main import zh_model, en_model
 
 text = """
 备受社会关注的湖南常德滴滴司机遇害案，将于1月3日9时许，在汉寿县人民法院开庭审理。此前，犯罪嫌疑人、19岁大学生杨某淇被鉴定为作案时患有抑郁症，为“有限定刑事责任能力”。
@@ -9,4 +9,18 @@ text = """
 田女士说，她相信法律，对最后的结果也做好心理准备。对方一家从未道歉，此前庭前会议中，对方提出了嫌疑人杨某淇作案时患有抑郁症的辩护意见。另具警方出具的鉴定书显示，嫌疑人作案时有限定刑事责任能力。
 新京报记者从陈师傅的家属处获知，陈师傅有两个儿子，大儿子今年18岁，小儿子还不到5岁。“这对我来说是一起悲剧，对我们生活的影响，肯定是很大的”，田女士告诉新京报记者，丈夫遇害后，他们一家的主劳动力没有了，她自己带着两个孩子和两个老人一起过，“生活很艰辛”，她说，“还好有妹妹的陪伴，现在已经好些了。”
 """
-ht.extract_keywords(text, 4)
+zh_model.extract_keywords(text, 4)
+
+text = """
+Supervised learning is the machine learning task of learning a function that
+maps an input to an output based on example input-output pairs. It infers a
+function from labeled training data consisting of a set of training examples.
+In supervised learning, each example is a pair consisting of an input object
+(typically a vector) and a desired output value (also called the supervisory signal). 
+A supervised learning algorithm analyzes the training data and produces an inferred function, 
+which can be used for mapping new examples. An optimal scenario will allow for the 
+algorithm to correctly determine the class labels for unseen instances. This requires
+the learning algorithm to generalize from the training data to unseen situations in a 
+'reasonable' way (see inductive bias).
+"""
+en_model.extract_keywords(text, 4)
